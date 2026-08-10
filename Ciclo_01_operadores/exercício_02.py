db660d6797cab983915cc00d5859789c09bb3f20
Bowie <lorenalrn22@gmail.com>
2026-08-10 15:09:21 -0400

    Resolvendo exercício 2.

---
+++ Ciclo_01_operadores/exercício_02.py
@@ -0,0 +1,11 @@
+'''Calcule o valor total da compra. Crie: 
+preço = 35
+quantidade = 4 
+'''
+
+preço = 35
+quantidade = 4
+
+total_gasto = preço * quantidade
+
+print(f" O valor total da compra foi de: {total_gasto}")
