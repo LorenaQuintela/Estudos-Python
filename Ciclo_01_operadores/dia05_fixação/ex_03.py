1e1788ab8b2943792eac66b97ee0db307bb408ac
Bowie <lorenalrn22@gmail.com>
2026-08-28 10:36:38 -0400

    Exercício de fixação 03 do dia 05

---
+++ Ciclo_01_operadores/dia05_fixação/ex_03.py
@@ -0,0 +1,13 @@
+'''Problema reverso
+Uma pessoa tinha uma determinada quantidade de dinheiro.
+Ela gastou R$ 135 e depois recebeu R$ 80. No final, ficou com R$ 545. 
+- Quanto dinheiro ela tinha inicialmente? '''
+
+gastou = 135
+recebeu = 80
+final = 545
+
+dinheiro = final - recebeu
+inicial = dinheiro + gastou
+
+print(inicial)
