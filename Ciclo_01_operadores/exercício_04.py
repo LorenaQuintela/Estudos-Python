aec23990597434945a79f8a26001d048d5bda441
Bowie <lorenalrn22@gmail.com>
2026-08-31 14:49:24 -0400

    Renomeia pastas de exercicios

--- Ciclo_01_operadores/exercício_04.py
+++
@@ -1,9 +0,0 @@
-'''Ex04: Uma empresa possui 57 funcionários. Ela quer formar grupos de 8 pessoas. Descubra quantos grupos completos podem ser formados? E quantas pessoas ficarão de fora.'''
-
-funcionarios = 57
-pessoas = 8
-
-resultado_grupo = funcionarios // pessoas
-print(f"Foram formados {resultado_grupo} grupos completos de 8 pessoas.")
-resultado_sobra = funcionarios % pessoas
-print(f"E {resultado_sobra} pessoa ficou de fora.")
--- Ciclo_01_operadores/exercício_07.py
+++
@@ -1,8 +0,0 @@
-''' Uma pessoa recebe R$ 2.000.00 reais de salário. Ela gasta 30% desse salário com o aluguel. Quanto ela gasta com aluguel?'''
-
-salario = 2000
-porcetagem = 30
-
-valor_descontado_aluguel = salario * porcetagem / 100
-
-print(f"O valor do aluguel é de: R$ {valor_descontado_aluguel:.2f} reais.")
